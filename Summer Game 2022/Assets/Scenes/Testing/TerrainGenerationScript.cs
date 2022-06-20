285c19342a778630e07f91ded6eb6fe649d6b1e2
SnprEaglez <vabhi2007@gmail.com>
2022-06-20 14:41:41 -0700

    Added sprites and updated script
    Added stone and dirt sprites
    
    Changed script so there would always be a layer of dirt on top of terrain

--- Summer Game 2022/Assets/Scenes/Testing/TerrainGenerationScript.cs
+++ Summer Game 2022/Assets/Scenes/Testing/TerrainGenerationScript.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 
 public class TerrainGenerationScript : MonoBehaviour
 {
-    public Sprite tile;
     public int worldSize = 100;
     public float noiseFreq = 0.05f;
     private float seed;
@@ -13,6 +12,10 @@ public class TerrainGenerationScript : MonoBehaviour
     public float caveFreq = 0.05f;
     public float TerrainFreq = 0.05f;
     public float CaveChance = 0.2f;
+    public int dirtlayerDepth = 5;
+    public Sprite grass;
+    public Sprite dirt;
+    public Sprite stone;
 
     private void Start()
     {
@@ -29,14 +32,38 @@ public class TerrainGenerationScript : MonoBehaviour
             float height = Mathf.PerlinNoise((x + seed) * TerrainFreq, seed * TerrainFreq) * HeightMultiplier + HeightAddition;
             for (int y = 0; y < height; y++)
             {
-                if (noiseTexture.GetPixel(x, y).r > CaveChance)
+                Sprite tileSprite;
+                if (y < height - dirtlayerDepth)
                 {
-                    GameObject newTile = new GameObject(name = "Terrain");
-                    newTile.transform.parent = this.transform;
-                    newTile.AddComponent<SpriteRenderer>();
-                    newTile.GetComponent<SpriteRenderer>().sprite = tile;
-                    newTile.transform.position = new Vector2(x + 0.5f, y + 0.5f);
+                    tileSprite = stone;
                 }
+                else
+                {
+                    tileSprite = dirt;
+                }
+                if (noiseTexture.GetPixel(x, y).r > CaveChance)  
+                    {
+                        GameObject newTile = new GameObject();
+                        newTile.transform.parent = this.transform;
+                        newTile.AddComponent<SpriteRenderer>();
+                        newTile.GetComponent<SpriteRenderer>().sprite = tileSprite;
+                        newTile.name = tileSprite.name;
+                        newTile.transform.position = new Vector2(x + 0.5f, y + 0.5f);
+                    }
+                else
+                {
+                    if (y > height - dirtlayerDepth)
+                    {
+                        tileSprite = dirt;
+                        GameObject newTile = new GameObject();
+                        newTile.transform.parent = this.transform;
+                        newTile.AddComponent<SpriteRenderer>();
+                        newTile.GetComponent<SpriteRenderer>().sprite = tileSprite;
+                        newTile.name = tileSprite.name;
+                        newTile.transform.position = new Vector2(x + 0.5f, y + 0.5f);
+                    }
+                }
+                    
             }
         }
     }
