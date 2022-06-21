46719fbddbde6fae91541595caeeb7ebc2c1ab2c
SnprEaglez <vabhi2007@gmail.com>
2022-06-21 11:15:33 -0700

    Updated script to generate chunks

--- Summer Game 2022/Assets/Scenes/Testing/TerrainGenerationScript.cs
+++ Summer Game 2022/Assets/Scenes/Testing/TerrainGenerationScript.cs
@@ -1,20 +1,24 @@
-using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class TerrainGenerationScript : MonoBehaviour
 {
-    public int worldSize = 200;
+    [Header("World Settings")]
+    public int worldSize = 320;
+    public int chunkSize = 16;
     public float noiseFreq = 0.03f;
     private float seed;
     public Texture2D noiseTexture;
 
+    [Header("Terrain and Cave Settings")]
     public float HeightMultiplier = 50f; //Steepness of terrain
     public float HeightAddition = 200; //Overall Height of terrain
     public float caveFreq = 0.05f; //Freq of caves (higher = more caves) (Used when making perlin texture; If higher, the noise in that area is more so more likely to make a cave)
     public float TerrainFreq = 0.02f; //Freq of terrain changes (greater = changes more often)
     public float CaveChance = 0.4f; //How dark/light a spot needs to be to become a cave (derived from perlin noise texture) (More = more caves)
 
-    public int dirtlayerDepth = 7;
+    [Header("Dirt, Grass, and Stone Settings")]
+    public int dirtlayerDepth = 5;
     public int BedRockLayerHeight = 5;
     public int DirtandStoneSeperationDistance = 5;
     public Sprite grass;
@@ -22,12 +26,29 @@ public class TerrainGenerationScript : MonoBehaviour
     public Sprite stone;
     public Sprite bedRock;
 
+    private GameObject[] worldChunks;
+    private List<Vector2> worldTiles = new List<Vector2>();
+
     private void Start()
     {
         seed = Random.Range(-10000, 10000);
         GenerateNoiseTexture();
+        GenerateChunks();
         GenerateTerrain();
        
+    }
+
+    public void GenerateChunks()
+    {
+        int numOfChunks = worldSize / chunkSize;
+        worldChunks = new GameObject[numOfChunks];
+        for (int i = 0; i < numOfChunks; i++)
+        {
+            GameObject newChunk = new GameObject();
+            newChunk.name = i.ToString();
+            newChunk.transform.parent = this.transform;
+            worldChunks[i] = newChunk;
+        }
     }
 
     public void GenerateTerrain()
@@ -97,15 +118,21 @@ public class TerrainGenerationScript : MonoBehaviour
         noiseTexture.Apply();
     }
 
-    public void PlaceTile(Sprite tileSprite, float x, float y)
+    public void PlaceTile(Sprite tileSprite,  int x, int y)
     {
         GameObject newTile = new GameObject();
-        newTile.transform.parent = this.transform;
+
+
+        float chunkCoord = (Mathf.Round(x / chunkSize) * chunkSize)/chunkSize;
+        newTile.transform.parent = worldChunks[(int)chunkCoord].transform;
+
         newTile.AddComponent<SpriteRenderer>();
         newTile.GetComponent<SpriteRenderer>().sprite = tileSprite;
         newTile.name = tileSprite.name;
         newTile.transform.position = new Vector2(x + 0.5f, y + 0.5f);
         newTile.AddComponent<BoxCollider2D>();
+
+        worldTiles.Add(newTile.transform.position - (Vector3.one * 0.5f));
     }
 }
 
