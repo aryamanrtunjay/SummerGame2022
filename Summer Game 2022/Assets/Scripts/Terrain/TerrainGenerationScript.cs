46b05d8a6bf2df2c94b7e802118d47f04e352e70
SnprEaglez <vabhi2007@gmail.com>
2022-07-10 00:06:57 -0700

    added lighting

--- Summer Game 2022/Assets/Scripts/Terrain/TerrainGenerationScript.cs
+++ Summer Game 2022/Assets/Scripts/Terrain/TerrainGenerationScript.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class TerrainGenerationScript : MonoBehaviour
 {
+
+    private float LightingTickCounter = 0;
+
     public Location location;
     public Camera Camera;
     public SpawnPoint SpawnPoint;
 
 
     private float seconds = 0;
-
+    private List<int> ActiveChunks = new List<int>();
 
 
     [Header("World Settings (world size must be divisible by chunk size)")]
@@ -134,12 +137,17 @@ public class TerrainGenerationScript : MonoBehaviour
             if (Vector2.Distance(new Vector2((i * chunkSize) + (chunkSize / 2), 0), new Vector2(location.x, 0)) > Camera.fieldOfView + 15)
             {
                 worldChunks[i].SetActive(false);
+
+                ActiveChunks.Remove(i);
+                
             }
             else
             {
                 worldChunks[i].SetActive(true);
+                ActiveChunks.Add(i);
             }
         }
+
     }
 
 
@@ -163,6 +171,17 @@ public class TerrainGenerationScript : MonoBehaviour
 
         RefreshChunks();
         //CheckTallGrassSurvivability();
+
+        int CurrentTime = (int)(LightingTickCounter);
+
+        if (CurrentTime % 5 == 0){
+            //Debug.Log("Updated Lighting");
+            UpdateLighting();
+        }
+
+        LightingTickCounter += 1*Time.deltaTime;
+
+
     }
 
 
@@ -341,6 +360,8 @@ public class TerrainGenerationScript : MonoBehaviour
 
         newTile.AddComponent<SpriteRenderer>();
         newTile.GetComponent<SpriteRenderer>().sprite = tileSprite;
+        newTile.AddComponent<TileInfo>();
+
         newTile.name = tileSprite.name;
         newTile.transform.position = new Vector2(x + 0.5f, y + 0.5f);
 
@@ -411,6 +432,68 @@ public class TerrainGenerationScript : MonoBehaviour
             Debug.Log("Invalid Direction : Function CheckNextTile");
         }
         return newCheckedTile;
+    }
+
+    public void UpdateLighting(){
+        //Updates light values
+        for (int i = 0; i < worldChunks.Length; i++)
+        {         
+            if (ActiveChunks.Contains(i))
+            {
+                for (int x = (i*chunkSize); x < ((i + 1)*chunkSize); x++){
+                   for (int y = worldSize-1; y >= 0; y--){
+                        
+                        //Commented code for when blank tiles are converted from null to an object
+                        // //If tile is in the top row
+                        // if (y == worldSize-1){
+                        //     worldTiles[x,y].GetComponent<TileInfo>().lightValue = 10;
+                        // }
+
+                        // //If current tile is a blank tile and not the top
+                        // else if (worldTiles[x,y] == null){
+                        //     if ((CheckNextTile("u", x, y )) == null){
+                        //         worldTiles[x,y].GetComponent<TileInfo>().lightValue = worldTiles[x,y + 1].GetComponent<TileInfo>().lightValue;
+                        //     }
+                        // }
+
+                        //If current tile is a terrain tile
+                        if (worldTiles[x,y] != null){
+                            if ((CheckNextTile("u", x, y )) == null){
+                                worldTiles[x,y].GetComponent<TileInfo>().lightValue = 10;
+                            }
+                            else{
+                                worldTiles[x,y].GetComponent<TileInfo>().lightValue = (worldTiles[x,y + 1].GetComponent<TileInfo>().lightValue) - 1;
+                            }
+
+                            if (worldTiles[x,y].GetComponent<TileInfo>().lightValue < 0){
+                                worldTiles[x,y].GetComponent<TileInfo>().lightValue = 0;
+                            }
+
+                            float CurrentTileLighting = (float)((worldTiles[x,y].GetComponent<TileInfo>().lightValue) * 0.1); 
+                            worldTiles[x,y].GetComponent<SpriteRenderer>().color = new Color(CurrentTileLighting,CurrentTileLighting,CurrentTileLighting);
+                        }
+
+                        
+
+                   }
+                }
+            }
+        }
+        // //Sets lighting
+        // for (int i = 0; i < worldChunks.Length; i++)
+        // {         
+        //     if (ActiveChunks.Contains(i))
+        //     {
+        //         for (int x = (i*chunkSize); x < ((i + 1)*chunkSize); x++){
+        //            for (int y = 0; y < worldSize; y++){
+        //                 if (worldTiles[x,y] != null){
+        //                     float CurrentTileLighting = (float)((worldTiles[x,y].GetComponent<TileInfo>().lightValue) * 0.1); 
+        //                     worldTiles[x,y].GetComponent<SpriteRenderer>().color = new Color(CurrentTileLighting,CurrentTileLighting,CurrentTileLighting);
+        //                 }
+        //            } 
+        //         }
+        //     }
+        // }
     }
 }
 
